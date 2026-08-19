952003d245802927c504fab1bdda980a0183506f
cchrisking <christou2c@gmail.com>
2026-08-19 12:10:18 +0200

    first commit

---
+++ src/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+app = Flask(__name__)
+@app.route("/")
+def main():
+    return '''
+     <form action="/echo_user_input" method="POST">
+         <input name="user_input">
+         <input age="user_age">
+         <input type="submit" value="Submit!">
+     </form>
+     '''
+@app.route("/echo_user_input", methods=["POST"])
+def echo_input():
+    input_text = request.form.get("user_input", "")
+    input_ageT = request.form.get("user_age", "")
+    return "You entered: " + input_text + " "+input_ageT
